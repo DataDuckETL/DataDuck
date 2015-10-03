0f9672a83524a0d43de149373b1c5f0e60c4bef8
J. R. Pickhardt <pickhardt@gmail.com>
2015-10-03 16:11:26 -0700

    Add validations.

--- README.md
+++ README.md
@@ -46,7 +46,9 @@ class Decks < DataDuck::Table
       "num_wins", "num_losses", "created_at", "updated_at",
       "is_drafted", "num_draft_wins", "num_draft_losses"]
 
-  transform :calculate_num_totals
+  transforms :calculate_num_totals
+
+  validates :validates_num_total
 
   output({
       :id => :integer,
@@ -68,6 +70,10 @@ class Decks < DataDuck::Table
     row[:num_total] = row[:num_wins] + row[:num_losses]
     row[:num_draft_total] = row[:num_draft_wins] + row[:num_draft_losses]
     row
+  end
+  
+  def validates_num_total(row)
+    return "Deck id #{ row[:id] } has negative value #{ row[:num_total] } for num_total." if row[:num_total] < 0
   end
 end
 ```
--- lib/dataduck/table.rb
+++ lib/dataduck/table.rb
@@ -4,14 +4,22 @@ module DataDuck
       attr_accessor :sources
       attr_accessor :output_schema
       attr_accessor :actions
+      attr_accessor :errors
     end
 
     attr_accessor :data
 
-    def self.transform(transformation_name)
+    def self.transforms(transformation_name)
       self.actions ||= []
       self.actions << [:transform, transformation_name]
     end
+    singleton_class.send(:alias_method, :transform, :transforms)
+
+    def self.validates(validation_name)
+      self.actions ||= []
+      self.actions << [:validate, validation_name]
+    end
+    singleton_class.send(:alias_method, :validate, :validates)
 
     def self.source(source_name, source_data = [])
       self.sources ||= {}
@@ -38,6 +46,8 @@ module DataDuck
 
     def extract!
       puts "Extracting table #{ self.name }..."
+
+      self.errors ||= []
       self.data = []
       self.class.sources.each_pair do |source, source_columns|
         import_query = "SELECT \"#{ source_columns.sort.join('","') }\" FROM #{ self.name }"
@@ -49,10 +59,18 @@ module DataDuck
 
     def transform!
       puts "Transforming table #{ self.name }..."
+
+      self.errors ||= []
       self.actions.each do |action|
         action_type = action[0]
+        action_method_name = action[1]
         if action_type == :transform
-          self.data.map! { |row| self.public_send(action[1], row) }
+          self.data.map! { |row| self.public_send(action_method_name, row) }
+        elsif action_type == :validate
+          self.data.each do |row|
+            error = self.public_send(action_method_name, row)
+            self.errors << error if !error.blank?
+          end
         end
       end
     end
