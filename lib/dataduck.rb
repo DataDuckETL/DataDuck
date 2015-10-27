f212b30c3ee93abdec2d56d6a46a36bb9e9eacd4
J. R. Pickhardt <pickhardt@gmail.com>
2015-10-27 12:47:44 -0700

    Allow running dataduck commands from further inside a project, by detecting the root Gemfile.

--- lib/dataduck.rb
+++ lib/dataduck.rb
@@ -20,7 +20,22 @@ module DataDuck
   spec = Gem::Specification.find_by_name("dataduck")
   create_module_var("gem_root", spec.gem_dir)
 
-  create_module_var("project_root", Dir.getwd)
+  detect_project_root = Dir.getwd
+  while true
+    if detect_project_root == ""
+      raise Exception.new("Could not find a Gemfile in the current working directory or any parent directories. Are you sure you're running this from the right place?")
+    end
+
+    if File.exist?(detect_project_root + '/Gemfile')
+      break
+    end
+
+    detect_project_root_splits = detect_project_root.split("/")
+    detect_project_root_splits = detect_project_root_splits[0..detect_project_root_splits.length - 2]
+    detect_project_root = detect_project_root_splits.join("/")
+  end
+  create_module_var("project_root", detect_project_root)
+
   create_module_var("config", {})
 
   dd_env_path = DataDuck.project_root + "/config/secret/#{ ENV['DATADUCK_ENV'] }.yml"
