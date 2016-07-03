0c99d1f1885a027e8806b28603b567f4b28c58e6
J. R. Pickhardt <pickhardt@gmail.com>
2016-07-03 13:30:06 -0700

    Update quickstart, version to 1.0.1

--- lib/dataduck/version.rb
+++ lib/dataduck/version.rb
@@ -2,7 +2,7 @@ module DataDuck
   if !defined?(DataDuck::VERSION)
     VERSION_MAJOR = 1
     VERSION_MINOR = 0
-    VERSION_PATCH = 0
+    VERSION_PATCH = 1
     VERSION = [VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH].join('.')
   end
 end
--- spec/dataduck_spec.rb
+++ spec/dataduck_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe DataDuck do
   it 'has the right version' do
-    expect(DataDuck::VERSION).to eq('1.0.0')
+    expect(DataDuck::VERSION).to eq('1.0.1')
   end
 end
