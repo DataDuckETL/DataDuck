376392491474b9c7cb8240e7892c9707092aec0a
J. R. Pickhardt <pickhardt@gmail.com>
2017-03-14 13:03:37 -0700

    Update version to 1.2.0

--- lib/dataduck/version.rb
+++ lib/dataduck/version.rb
@@ -1,7 +1,7 @@
 module DataDuck
   if !defined?(DataDuck::VERSION)
     VERSION_MAJOR = 1
-    VERSION_MINOR = 1
+    VERSION_MINOR = 2
     VERSION_PATCH = 0
     VERSION = [VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH].join('.')
   end
--- spec/dataduck_spec.rb
+++ spec/dataduck_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe DataDuck do
   it 'has the right version' do
-    expect(DataDuck::VERSION).to eq('1.1.0')
+    expect(DataDuck::VERSION).to eq('1.2.0')
   end
 end
